5a746926d3d3862a8d99bc4e102110e874386162
whitehat92 <ze_correia93@hotmail.com>
2019-09-12 10:37:24 +0100

    Update osinter.py

--- osinter.py
+++ osinter.py
@@ -11,16 +11,31 @@ import webbrowser
 import os
 from github import Github
 import socket
+from random import randint
 from re import findall
 from requests import get
 from re import search
 import urllib.request
+import csv
+from pylatex import Document, Section, Math, Tabular, Figure, SubFigure, \
+    Package, TikZ, Axis, Plot, Itemize, Enumerate, Description, MultiColumn, \
+    MultiRow, Command, Matrix, VectorName, Quantity, TableRowSizeError, \
+    LongTable, FlushLeft, FlushRight, Center, MiniPage, TextBlock, \
+    PageStyle, Head, Foot, StandAloneGraphic, Tabularx, ColumnType, NewLine, \
+    LineBreak, NewPage, HFill, HugeText, LargeText, MediumText, \
+    SmallText, FootnoteText, TextColor, FBox, MdFramed, Tabu, \
+    HorizontalSpace, VerticalSpace, NoEscape, Table, LongTabu, Subsection
+from pylatex.utils import escape_latex, fix_filename, dumps_list, bold, \
+    italic, verbatim
+import googlesearch
+import bs4
+import selenium.webdriver as webdriver
 
 
 
 input_search = input("What do you want to search? ")
 ip = get('https://api.ipify.org').text
-geolocation = get("https://geo.ipify.org/api/v1?apiKey=&ipAddress=" + ip).text
+geolocation = get("https://geo.ipify.org/api/v1?apiKey=&ipAddre     ss=" + ip).text
 
 geolocationip = urllib.request.urlopen("https://geoip-db.com/jsonp/" + input_search)
 data = geolocationip.read().decode()
@@ -30,30 +45,31 @@ data = data.split("(")[1].strip(")")
 print("Please bear in mind that now your Public IP is " + ip + " " + ", located in " + "\n" + data)
 
 # Search Shodan
-api_key = "YFJ1LyOqk5KEAvxIiulVBOoPqbVMFHR5"
+api_key = "KEY"
 shodan_api = shodan.Shodan(api_key)
 results = shodan_api.search(input_search)
-
-    # Show the results
+# Show the results
 print("-------------------------------| SHODAN RESULTS |-------------------------------")
 print('Results found: {}'.format(results['total']))
-for result in results['matches']:
-    print('IP: {}'.format(result['ip_str']))
-    print(result['data'])
+shody = results['matches']
+for resultshodan in shody:
+    print('IP: {}'.format(resultshodan['ip_str']))
+    print(resultshodan['data'])
     print('')
 
 
     #CENSYS
 API_URL = "https://censys.io/api/v1"
 UID = "8d439ff9-7b2c-46cc-af3b-350a0077a15f"
-SECRET = "ySzM6ohyVtXcFRpcXopbdDGs9S3q3tkJ"
+SECRET = "KEY"
 
 print("--------------------------------| CENSYS RESULTS |--------------------------------------")
 try:
     raw_response_ip = socket.gethostbyname(input_search)
     raw_response = get('https://censys.io/ipv4/' + raw_response_ip + '/raw').text
-    printavel = raw_response.replace('&#34;', '"')
-    print(printavel)
+    censys_clean = raw_response.replace('&#34;', '"')
+    #print(censys_clean)
+    print(censys_clean)
 except:
     pass
 
@@ -68,8 +84,8 @@ if res.status_code != 200:
 """
 print("----------------------------------| ROBTEX RESULTS |----------------------------------")
 print("printing pdns (PowerDNS |-| A CACHING DNS PROXY SERVER) forward results...")
-response = robtex_python.pdns_forward(input_search)
-print(response)
+pdns_forward = robtex_python.pdns_forward(input_search)
+print(pdns_forward)
 print("''''''''''printing IP of the search'''''''''''")
 responseip = robtex_python.ip_query(input_search)
 print(responseip)
@@ -84,7 +100,7 @@ print(responsepdnsreverse)
 
 print("ZOOMEYE RESULTS")
 #ZOOMEYE
-#my access token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGl0eSI6InplX2NvcnJlaWE5M0Bob3RtYWlsLmNvbSIsImlhdCI6MTU2NDI1MzU0MywibmJmIjoxNTY0MjUzNTQzLCJleHAiOjE1NjQyOTY3NDN9.Ghw8moQfhR0qzMMv4U8-prGrOImQ6i7vjtUkqzh_r28"
+#my access token: KEY"
 #['ZoomEye', '__builtins__', '__doc__', '__file__', '__name__', '__package__', 'getpass', 'requests', 'show_ip_port', 'show_site_ip', 'zoomeye_api_test']
 #zm = zoomeye.ZoomEye()
 #print(zm)
@@ -98,11 +114,11 @@ print("ZOOMEYE RESULTS")
 
 print("--------------------------------FULL CONTACT------------------------------")
 
-APIkey:"wpSJrr7wVZNEnD6zFjRkUMFo5ijajWRO"
+APIkey:"KEY"
 
 try:
     req = urllib.request.Request('https://api.fullcontact.com/v3/'+input_search, method='post')
-    req.add_header('Authorization', 'Bearer wpSJrr7wVZNEnD6zFjRkUMFo5ijajWRO ')
+    req.add_header('Authorization', 'Bearer KEY ')
     data = json.dumps([])
     response = urllib.request.urlopen(req)
 except:
@@ -121,22 +137,22 @@ application.get_profile()
 GET https://api.linkedin.com/v2/{input_search}
 """
 print("--------------------------------- FACEBOOK DATA ----------------------------")
-token = "EAAEnQV7kXCcBAEbMc436liLoF3NqqOXQK3faaIZC4lZBtqeTYUoZCvvRQBVibeJ5WjUAWCdhlO36jAD52dOAyN4WvmM1X0jLrR4wXD9WuS6EecJQJajykIOTrzPxLItXgZADUKab4tAHAm5fZAA7BJ321s5mA8dBF5xz9QLIpmZBC2GFGvp4xuvqZAU7LTldxBRNM0yPuOSsQZDZD"
+token = "KEY"
 fb_request_private = requests.get("https://graph.facebook.com/v2.9/search?q=" + input_search + "&type=user&access_token=" + token)
 print(fb_request_private)
 print("--------- PRINTING DATA FROM FACEBOOK PUBLICALLY ---------------------------")
 fb_request_public = requests.get("https://facebook.com/public/" + input_search)
 print(fb_request_public.headers)
-fb_just_me = requests.get("https://graph.facebook.com/v3.3/me?fields=id%2Cname%2Cphotos&access_token=EAAEnQV7kXCcBAEbMc436liLoF3NqqOXQK3faaIZC4lZBtqeTYUoZCvvRQBVibeJ5WjUAWCdhlO36jAD52dOAyN4WvmM1X0jLrR4wXD9WuS6EecJQJajykIOTrzPxLItXgZADUKab4tAHAm5fZAA7BJ321s5mA8dBF5xz9QLIpmZBC2GFGvp4xuvqZAU7LTldxBRNM0yPuOSsQZDZD").text
+fb_just_me = requests.get("https://graph.facebook.com/v3.3/me?fields=id%2Cname%2Cphotos&access_token=KEY").text
 fb_just_me
 try:
-    fb_searchbycurl = os.system("C:/Users/Antonio/Desktop/curl/curl.exe -X GET" + " " + "https://graph.facebook.com/v3.3/me?fields=id%2Cname%2Cphotos&access_token=EAAEnQV7kXCcBAEbMc436liLoF3NqqOXQK3faaIZC4lZBtqeTYUoZCvvRQBVibeJ5WjUAWCdhlO36jAD52dOAyN4WvmM1X0jLrR4wXD9WuS6EecJQJajykIOTrzPxLItXgZADUKab4tAHAm5fZAA7BJ321s5mA8dBF5xz9QLIpmZBC2GFGvp4xuvqZAU7LTldxBRNM0yPuOSsQZDZD")
+    fb_searchbycurl = os.system("C:/pathtocurl -X GET" + " " + "https://graph.facebook.com/v3.3/me?fields=id%2Cname%2Cphotos&access_token=KEY")
     fb_searchbycurl
 except:
     pass
 
 print("-------------------------------------- GITHUB API ---------------------------------")
-g = Github("2db60eef1c76f94005cb204b12207958b71fe3ee ")
+g = Github("KEY ")
 
 #for repo in g.get_repos():
  #   print(repo.name)
@@ -144,19 +160,19 @@ g = Github("2db60eef1c76f94005cb204b12207958b71fe3ee ")
 print("---- REVERSE IP LOOKUP OF YOUR SEARCH")
 #print("This one was not properly tested. If nothing shows up, change something....")
 lookup = 'https://api.hackertarget.com/reverseiplookup/?q=' + input_search
-result = get(lookup).text
-print(result)
+resultlookup = get(lookup).text
+print(resultlookup)
 
 
 print("-------------- NAME SERVER LOOKUP -------------------------- ")
 #print("This one was not properly tested. If nothing shows up, change something....")
-result = get('http://api.hackertarget.com/dnslookup/?q=' + input_search).text
-print(result)
+resultnameserver = get('http://api.hackertarget.com/dnslookup/?q=' + input_search).text
+print(resultnameserver)
 print("--------------------------------- LET'S NOW FIND OUT WHICH TECHNOLOGIES WE ARE TALKING ABOUT -----------------------")
 #print("This one was not properly tested. If nothing shows up, change something....")
 if "https" in input_search:
     input_search.replace("https://", "")
-data = get('https://api.wappalyzer.com/lookup-basic/beta/?url=' + input_search).text
+data = get('https://api.larger.io/v1/search/key/KEY?domain=' + input_search).text
 jsoned_data = json.loads(data)
 technologies = []
 print(jsoned_data)
@@ -170,7 +186,7 @@ for tech in technologies:
 print("----------------- NOW IF THIS FUCKER IS USING A CMS, LET'S FIND OUT ------------------------")
 #print("This one was not properly tested. If nothing shows up, change something....")
 
-response = get('https://whatcms.org/?gpreq=json&jsoncallback=jQuery1124008091494457806547_1554361369057&s=%s&na=&nb=1cg805dlm7d7e5eickf67rzxrn12mju6bnch3a99hrt88v7n8rhf0lovwr8d0zm1&verified=&_=1554361369059' + input_search).text
+response = get('https://whatcms.org/?gpreq=json&jsoncallback=jQueryKEY&s=%s&na=&nb=KEY&verified=&_=KEY' + input_search).text
 match = search(r'uses<\\/div>[^>]+>(.*?)<\\/a>', response)
 print(match)
 #print(good + ' ' + match.group(1) + '\n')
@@ -179,8 +195,8 @@ print(match)
 
 print("------------------------------ SUBDOMAINS FOUND --------------------------------")
 #print("This one was not properly tested. If nothing shows up, change something....")
-response = get('https://findsubdomains.com/subdomains-of/' + input_search).text
-print(response)
+subdomains = get('https://findsubdomains.com/subdomains-of/' + input_search).text
+print(subdomains)
 
 """
 matches = findall(r'(?s)<div class="domains js-domain-name">(.*?)</div>', response)
@@ -189,14 +205,36 @@ for match in matches:
     print(cleanresponse)
 """
 
-print("--------------- Exploring Google World ------------------------- still trying")
-#GOOGLE
+print("VIRUSTOTAL")
+vt = urllib.request.urlopen("https://www.virustotal.com/gui/domain/" + input_search + "/relations")
+vtjson = vt.read().decode()
+print(vtjson)
+
+print("--------------- Exploring Google World -------------------------")
+query = input_search
 try:
-    req = urllib.request.Request("https://www.google.com/search?ei=kho6XZakEY2xULj4uvAE&q=" + input_search + "&oq=" + input_search + "&gs_l=psy-ab.3...24584.26899..27013...0.0..0.0.0.......0....1..gws-wiz.....0..0i71.99Zu33H4yCI&ved=0ahUKEwiWtcq__dDjAhWNGBQKHTi8Dk4Q4dUDCAo&uact=5")
-    print(req)
+    searchgoogle = googlesearch.search(query, tld='com', lang='en', tbs='0', safe='off', num=10, start=0, stop=None, domains=None, pause=4.0, only_standard=False, extra_params={}, tpe='', user_agent="Mozilla/10.0")
+    for item in searchgoogle:
+        print(item)
 except:
     pass
-
+"""
+HOW TO WORK WITH GOOGLE SEARCH: vv
+query (str) – Query string. Must NOT be url-encoded.
+tld (str) – Top level domain.
+lang (str) – Language.
+tbs (str) – Time limits (i.e “qdr:h” => last hour, “qdr:d” => last 24 hours, “qdr:m” => last month).
+safe (str) – Safe search.
+num (int) – Number of results per page.
+start (int) – First result to retrieve.
+or None stop (int) – Last result to retrieve. Use None to keep searching forever.
+of str or None domains (list) – A list of web domains to constrain the search.
+pause (float) – Lapse to wait between HTTP requests. A lapse too long will make the search slow, but a lapse too short may cause Google to block your IP. Your mileage may vary!
+only_standard (bool) – If True, only returns the standard results from each page. If False, it returns every possible link from each page, except for those that point back to Google itself. Defaults to False for backwards compatibility with older versions of this module.
+of str to str extra_params (dict) – A dictionary of extra HTTP GET parameters, which must be URL encoded. For example if you don’t want Google to filter similar results you can set the extra_params to {‘filter’: ‘0’} which will append ‘&filter=0’ to every query.
+tpe (str) – Search type (images, videos, news, shopping, books, apps) Use the following values {videos: ‘vid’, images: ‘isch’, news: ‘nws’, shopping: ‘shop’, books: ‘bks’, applications: ‘app’}
+or None user_agent (str) – User agent for the HTTP requests. Use None for the default.
+"""
 google_dorks = input("Do you want to use Google dorks? ")
 if google_dorks == "" or google_dorks == "y" or google_dorks == "Y":
     printlist = input("Do you know which parameters are available? ")
@@ -219,11 +257,11 @@ if google_dorks == "" or google_dorks == "y" or google_dorks == "Y":
 browseropener = input("Do you want to open the browser for the rest of the search engines (25)? (y/n) ")
 if browseropener == "y" or browseropener == "Y" or browseropener == "":
     print("--------------------- BING  ----------------------")
-    webbrowser.open_new("https://www.bing.com/search?q=antonio-correia.com&qs=n&form=QBLH&sp=-1&pq=" + input_search + ".co&sc=0-18&sk=&cvid=855BB34029354D538EC5F30BF05675CA")
+    webbrowser.open_new("https://www.bing.com/search?q=" + input_search + "&qs=n&form=QBLH&sp=-1&pq=" + input_search + ".co&sc=0-18&sk=&cvid=KEY")
     print("------------------------- DUCKDUCKGO  -----------------------")
     webbrowser.open_new("https://duckduckgo.com/?q=" + input_search + "&t=h_&ia=web")
     print("--------------------- GOOGLE ----------------------")
-    webbrowser.open_new("https://www.google.com/search?ei=kho6XZakEY2xULj4uvAE&q=" + input_search + "&oq=" + input_search + "&gs_l=psy-ab.3...24584.26899..27013...0.0..0.0.0.......0....1..gws-wiz.....0..0i71.99Zu33H4yCI&ved=0ahUKEwiWtcq__dDjAhWNGBQKHTi8Dk4Q4dUDCAo&uact=5")
+    webbrowser.open_new("https://www.google.com/search?ei=KEY" + input_search + "&oq=" + input_search + "&gs_l=KEY&ved=KEY&uact=5")
     print("-------------------------- ZOOMEYE opening browser ------------------------------")
     webbrowser.open_new('https://www.zoomeye.org/searchResult?q=' + input_search)
     print(
@@ -274,6 +312,48 @@ if browseropener == "y" or browseropener == "Y" or browseropener == "":
 
 else:
     pass
+    if __name__ == '__main__':
+        doc = Document()
+        doc.preamble.append(Command('title', input_search))
+        doc.preamble.append(Command('author', 'YOURNAME'))
+        doc.preamble.append(Command('date', NoEscape(r'\today')))
+        doc.append(NoEscape(r'\maketitle'))
+        with doc.create(Section('Searching')):
+            doc.append('Analyzis of ' + " " + input_search)
+        with doc.create(Section('Name server lookup')):
+            doc.append(resultnameserver)
+        with doc.create(Section("Reverse IP lookup")):
+            doc.append(resultlookup)
+            #doc.append(italic('italic text. '))
+        with doc.create(Section('Subdomains')):
+            doc.append('Analyzis of ' + " " + subdomains)
+        with doc.create(Section("Technologies")):
+            doc.append(jsoned_data)
+        with doc.create(Section("Censys")):
+            #doc.append(censys_clean)
+            doc.append(censys_clean)
+        with doc.create(Section("VIRUSTOTAL")):
+            doc.append(vtjson)
+        with doc.create(Section("What's said on Google")):
+            doc.append(searchgoogle)
+            #with doc.create(Subsection('A subsection')):
+             #   doc.append('Also some crazy characters: $&#{}')
+        # Document with `\maketitle` command activated
+        doc.append(NewPage)
+        with doc.create(Section("Table1")):
+            with doc.create(Tabular(table_spec='|l|l|')) as table1:
+                table1.add_hline()
+                table1.add_row("Name Server Lookup", MultiColumn(1, align=NoEscape(r'p{11cm}|'), data=resultnameserver))
+                table1.add_hline()
+                table1.add_row("Robtex", MultiColumn(1, align=NoEscape(r'p{16cm}|'), data=pdns_forward))
+                table1.add_hline()
+                table1.add_row("Robtex1", MultiColumn(1, align=NoEscape(r'p{16cm}|'), data=responseip))
+                table1.add_hline()
+                table1.add_row("Robtex2", MultiColumn(1, align=NoEscape(r'p{16cm}|'), data=responseas))
+                table1.add_hline()
+                table1.add_row("Tech", MultiColumn(1, align=NoEscape(r'p{16cm}|'), data=jsoned_data))
+                table1.add_hline()
+        doc.generate_pdf('osinter', clean_tex=False)
 
 """
 --- Services
@@ -306,13 +386,13 @@ IP's, one should look for the ASN to find out exactly to which location or zone 
 
 databreaches.net
 https://www.searchencrypt.com/
-europeliveuamap.com - base de dados espacial. entrar por liveuamap.com e escolher o país, senao somos barrados pela membership
+europeliveuamap.com - spatial database
 https://nuclearsecrecy.com/nukemap/
 https://www.n2yo.com/
 https://wifispc.com/
 freekeywordresearch.com
 keyworddiscovery.com
-https://www.bincodes.com/bin-search/ --> procurar os bins ou identificadores bancários
+https://www.bincodes.com/bin-search/ --> Bins
 pic2map.com
 https://www.pic2map.com/
 https://wigle.net/
@@ -322,25 +402,43 @@ https://www.boatinfoworld.com/
 http://www.internationalcrimesdatabase.org/
 vat-lookup.co.uk
 https://bitcoinwhoswho.com/
-https://www.dgmarket.com/ --> concursos públicos, em portugal é o portalbase
-https://www.commercial-register.sg.ch/ --> registars das empresas
+https://www.dgmarket.com/ --> work
+https://www.commercial-register.sg.ch/ --> registrars
 https://pt.kompass.com/ 
-https://knowem.com/ --> procurar pessoas, pipl.com também
+https://knowem.com/ --> people search
 start.me
-https://start.me/p/VRxaj5/dating-apps-and-hook-up-sites-for-investigators --> agregador de várias redes de dating
-http://irc.netsplit.de/channels/ --> procurar no irc pelo tema desejado
+https://start.me/p/VRxaj5/dating-apps-and-hook-up-sites-for-investigators --> dating
+http://irc.netsplit.de/channels/ --> IRC
 http://searchlr.net/ --> tumblr search engine
-resavr.com --> reddit search, coisas que foram apagadas
-https://codeofaninja.com/tools/find-instagram-user-id --> procurar pelo id do user
+resavr.com --> REDDIT deleted
+https://codeofaninja.com/tools/find-instagram-user-id --> instagram, for ID
 http://www.geocreepy.com/
 https://iotscanner.bullguard.com/
-https://millionshort.com/ --> procurar resultados que estão na 15ªa página
+https://millionshort.com/ --> google last page
 similarsites.com
 https://www.searchftps.net/
-etools.ch --> metasearc
+etools.ch --> metasearch
 """
 
 
 #error handling
 #Exception, <variablename_toexpress the error:
 #print(str(variablename_toexpress the error)
+
+
+#Latex code
+"""
+if __name__ == '__main__':
+    doc = Document()
+with doc.create(Section("Table2")):
+    with doc.create(Tabular(table_spec='|c|l|')) as table2:
+        table2.add_hline
+        # table2.add_row("Vuln", MultiColumn(1, align=NoEscape(r'p{11.5cm}|'), data=x))
+        table2.add_row("Description", MultiColumn(1, align=NoEscape(r'p{11.5cm}|'), data=variable))
+        table2.add_hline()
+        table2.add_row("CVE", MultiColumn(1, align=NoEscape(r'p{11.5cm}|'), data=item))
+        table2.add_hline()
+        table2.add_row("Medium", MultiColumn(1, align=NoEscape(r'p{11.5cm}|'), data=y))
+        table2.add_hline()
+doc.generate_pdf('osinter', clean_tex=False)
+"""
